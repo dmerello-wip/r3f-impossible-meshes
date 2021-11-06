e51cae59137f3235e37aa59238dc9c26b1c2c603
Diego Merello <diego@wipitalia.it>
2021-11-06 21:54:25 +0100

    using Leva to set positions

--- src/components/ImpossibleTriangle.jsx
+++ src/components/ImpossibleTriangle.jsx
@@ -1,43 +1,45 @@
-// const ImpossibleTriangle = ({ color }) => {
-//   const mesh = useRef(null)
-//   const [hovered, setHover] = useState(false)
-//   useFrame((state, delta) =>
-//     mesh.current
-//       ? (mesh.current.rotation.y = mesh.current.rotation.x += 0.01)
-//       : null
-//   )
-//   return (
-//     <>
-//       <mesh
-//         ref={mesh}
-//         onClick={() => router.push('fava')}
-//         onPointerOver={() => setHover(true)}
-//         onPointerOut={() => setHover(false)}
-//         scale={hovered ? 1.1 : 1}
-//       >
-//         <boxBufferGeometry args={[1, 1, 1]} />
-//         <meshPhysicalMaterial color={color} />
-//       </mesh>
-//     </>
-//   )
-// }
-// export default ImpossibleTriangle
-
-
+import * as THREE from 'three'
 import React, { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
+import { useControls } from 'leva'
 
 export default function ImpossibleTriangle({ ...props }) {
   const group = useRef()
+  const finalRotation = { x: 125.30, y: 180, z: -44.9 }
   const { nodes, materials } = useGLTF('/models/triangle.gltf')
+  const { rotX, rotY, rotZ } = useControls({
+    rotX: finalRotation.x,
+    rotY: finalRotation.y,
+    rotZ: finalRotation.z,
+  })
+
   return (
-      // <group rotation={[-Math.PI, 0, -Math.PI]} position={[0, 0, 0]} ref={group} {...props} dispose={null}>
-      <group rotation={[0, 0, 0]} position={[0, 0, 0]} ref={group} {...props} dispose={null}>
-        <mesh geometry={nodes.mesh_0.geometry} material={materials['Material.001']} />
-        <mesh geometry={nodes.mesh_1.geometry} material={materials['Material.002']} />
-        <mesh geometry={nodes.mesh_2.geometry} material={materials['Material.003']} />
-      </group>
+    <group
+    position={[5,-5,0]}
+      rotation={[
+        THREE.Math.degToRad(rotX),
+        THREE.Math.degToRad(rotY),
+        THREE.Math.degToRad(rotZ),
+      ]}
+      ref={group}
+      {...props}
+      dispose={null}
+      scale={[1, 1, 1]}
+    >
+      <mesh
+        geometry={nodes.mesh_0.geometry}
+        material={materials['Material.001']}
+      />
+      <mesh
+        geometry={nodes.mesh_1.geometry}
+        material={materials['Material.002']}
+      />
+      <mesh
+        geometry={nodes.mesh_2.geometry}
+        material={materials['Material.003']}
+      />
+    </group>
   )
 }
 
--- src/components/LCanvas.jsx
+++ src/components/LCanvas.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera, OrbitControls, Preload } from '@react-three/drei'
+import { PerspectiveCamera, OrthographicCamera ,OrbitControls, Preload } from '@react-three/drei'
 import useStore from '@/store/store'
 
 const LCanvas = ({ children , orbitControlActive, cameraPosition}) => {
@@ -12,9 +12,11 @@ const LCanvas = ({ children , orbitControlActive, cameraPosition}) => {
             onCreated={(state) => state.events.connect(dom.current)}
         >
           {orbitControlActive && <OrbitControls camera={cameraRef.current} /> }
-          <PerspectiveCamera castShadow
-            position={[0,50,0]}
+          <OrthographicCamera castShadow
+            zoom={30}
+            position={[0,0,20]}
             makeDefault={true}
+            frustumCulled = {false}
             ref={cameraRef}
           />
           <Preload all />
