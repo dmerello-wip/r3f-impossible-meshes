5de3ec83d0b50676696db3b9b475a410b2c153f3
Diego Merello <diego@wipitalia.it>
2021-11-06 22:38:14 +0100

    first animatin experiments

--- src/components/ImpossibleTriangle.jsx
+++ src/components/ImpossibleTriangle.jsx
@@ -1,28 +1,53 @@
 import * as THREE from 'three'
-import React, { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
+import React, { useEffect, useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
-import { useControls } from 'leva'
+import { gsap, Power2 } from "gsap";
+// import { useControls } from 'leva'
 
 export default function ImpossibleTriangle({ ...props }) {
-  const group = useRef()
+
+  const obj = useRef()
   const finalRotation = { x: 125.30, y: 180, z: -44.9 }
   const { nodes, materials } = useGLTF('/models/triangle.gltf')
-  const { rotX, rotY, rotZ } = useControls({
-    rotX: finalRotation.x,
-    rotY: finalRotation.y,
-    rotZ: finalRotation.z,
-  })
+  // const { rotX, rotY, rotZ } = useControls({
+  //   rotX: finalRotation.x,
+  //   rotY: finalRotation.y,
+  //   rotZ: finalRotation.z,
+  // })
+
+  const startAnimation = (animationType)=> {
+    let animationParams = {
+      x: finalRotation.x,
+      z: finalRotation.z
+    };
+    gsap.from(animationParams, {
+      x : 180,
+      z : -70,
+      duration: 2,
+      ease: Power2.easeOut,
+      onUpdate: () => {
+        obj.current.rotation.x = THREE.Math.degToRad(animationParams.x);
+        obj.current.rotation.z = THREE.Math.degToRad(animationParams.z);
+      }
+    });
+  };
+
+
+  useEffect(()=>{
+    startAnimation();
+  }, []);
+
+
 
   return (
     <group
-    position={[5,-5,0]}
+      position={[5,-5,0]}
       rotation={[
-        THREE.Math.degToRad(rotX),
-        THREE.Math.degToRad(rotY),
-        THREE.Math.degToRad(rotZ),
+        THREE.Math.degToRad(finalRotation.x),
+        THREE.Math.degToRad(finalRotation.y),
+        THREE.Math.degToRad(finalRotation.z),
       ]}
-      ref={group}
+      ref={obj}
       {...props}
       dispose={null}
       scale={[1, 1, 1]}
