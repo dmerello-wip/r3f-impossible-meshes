7ea2ddd20a61f235acecb2b9b36fc9bfe462aaaa
Diego Merello <diego@wipitalia.it>
2022-03-24 20:17:42 +0100

    preparing animation states

--- src/components/Hero/Index.jsx
+++ src/components/Hero/Index.jsx
@@ -6,20 +6,29 @@ import {useState} from "react";
 
 export default function Hero({contents}) {
 
-  const [index, setIndex] = useState(0);
-  const [slideData, setSlideData] = useState(contents[index]);
+  const slidesData = contents.map((elem,i)=> {
+    elem.index = i+1;
+    return elem;
+  });
+
+  const [slideData, setSlideData] = useState(slidesData[0]);
+  const [animationState, setAnimationState] = useState('in'); // values: out, in
+
+  const changeHandler = (nextSlideIndex)=>{
+    // animate out
+  };
 
   return (
     <div className='c-hero'>
     <div className='c-hero__canvas'>
       <Scene orbitControlActive>
-        {(slideData.model==='DoubleTriangle') && <DoubleTriangle />}
-        {(slideData.model==='Triangle') && <Triangle />}
-        {(slideData.model==='Cube') && <Cube />}
+        {(slideData.model==='DoubleTriangle') && <DoubleTriangle animationState={animationState} />}
+        {(slideData.model==='Triangle') && <Triangle animationState={animationState} />}
+        {(slideData.model==='Cube') && <Cube animationState={animationState} />}
       </Scene>
     </div>
-      <div className='c-hero__content'>
-        <div className="c-hero__index">{index}</div>
+      <div className='c-hero__content' data-animation={animationState}>
+        <div className="c-hero__index">{slideData.index}</div>
         <div className='c-hero__pretitle'>{slideData.pretitle}</div>
         <div className='c-hero__title'>{slideData.title}</div>
         <div className='c-hero__subtitle'>{slideData.subtitle}</div>
